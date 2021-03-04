e7a6a0686851c890313202f4adfd3026472f6ac4
Lucas <lucas@Lucass-iMac.local>
2021-03-04 16:43:51 -0500

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+});
+
+const server = app.listen(process.env.PORT || 3001, () => {
+    console.log('Listening...');
+});
