fbbd4c04e86e4b28eb3d1bdd15082e99d2b4e43d
Matthew Tory <matthew.tory@mail.utoronto.ca>
2021-03-05 11:25:43 -0500

    Update chat

--- backend/chat.js
+++ backend/chat.js
@@ -9,17 +9,22 @@ const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_A
 exports.getChatToken = async (req, res) => {
     const {
         identity,
-        name
+        name,
+        picture
     } = req.body;
 
     try {
-        await twilioClient.chat.services(process.env.TWILIO_CHAT_SERVICE_SID).users.get(identity).fetch();
+        const user = twilioClient.chat.services(process.env.TWILIO_CHAT_SERVICE_SID).users.get(identity);
+        await user.update({
+            attributes: JSON.stringify({ name, picture })
+        })
     } catch(e) {
         await twilioClient.chat.services(process.env.TWILIO_CHAT_SERVICE_SID).users.create({
             identity,
             friendlyName: name,
             attributes: {
-                name
+                name,
+                picture
             }
         })
     }
@@ -34,52 +39,8 @@ exports.getChatToken = async (req, res) => {
         process.env.TWILIO_API_SECRET
     );
 
-    console.log(
-        process.env.TWILIO_ACCOUNT_SID,
-        process.env.TWILIO_API_KEY,
-        process.env.TWILIO_API_SECRET
-    );
-
     token.addGrant(chatGrant);
     token.identity = identity;
 
     res.json({ token: token.toJwt() });
 };
-
-exports.getMessages = async (req, res) => {
-    const results = await client.send(new ScanCommand({
-        TableName: 'Messages'
-    }));
-
-    res.status(200).json(results.Items.map(message => ({
-        MessageContent: message.MessageContent.S,
-        MessageTimestamp: message.MessageTimestamp.N,
-        MessageId: message.MessageId.S
-    })));
-};
-
-exports.addMessage = async (req, res) => {
-    const {
-        message
-    } = req.body;
-
-    if (typeof message === 'string' && message.trim().length > 0) {
-        const newMessage = {
-            MessageContent: { S: message.trim() },
-            MessageId: { S: uuid.v1() },
-            MessageTimestamp: { N: `${(new Date()).getTime()}` }
-        };
-
-        await client.send(new PutItemCommand({
-            TableName: 'Messages',
-            Item: newMessage
-        }));
-
-        await exports.getMessages(req, res);
-    } else {
-        res.status(400).json({
-            error: 'Invalid message'
-        });
-    }
-};
-
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,51 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import FacebookLogin from 'react-facebook-login';
 import Room from "./apps/Room";
 import './App.css';
 import {store} from "./index";
 import {initializeChat} from "./redux/chat/chat";
 
+
 function App() {
-  const [login, setLogin] = useState(false);
-  const [data, setData] = useState({});
+    const [login, setLogin] = useState(false);
+    const [data, setData] = useState({});
 
-  const responseFacebook = (response) => {
-    console.log(response);
-    setData(response);
-    if (response.accessToken) {
-      setLogin(true);
-    } else {
-      setLogin(false);
-    }
-    initializeChat(store, {
-      identity: `facebook_uid ${response.id}`,
-      name: `${response.name}`,
-      data: response
-    });
-  }
+    const responseFacebook = async (response) => {
+        setData(response);
 
-  return (
-    <div>
-    <div>
-          { !login &&
-            <FacebookLogin
-              appId="785768468241627"
-              autoLoad={true}
-              fields="name,email,picture"
-              scope="public_profile,user_friends"
-              callback={responseFacebook}
-              icon="fa-facebook" />
-          }
-    </div>
-    <div>
-        { login &&
-          <Room/>
+        if (response.accessToken) {
+            await initializeChat(store, {
+                identity: `${response.id}`,
+                name: `${response.name}`,
+                data: response
+            });
+
+            setLogin(true);
+        } else {
+            setLogin(false);
         }
-    </div>
-    </div>
-  );
+    }
+
+    return (
+        <div>
+            <div>
+                {!login &&
+                <FacebookLogin
+                    appId="785768468241627"
+                    autoLoad={true}
+                    fields="name,email,picture"
+                    scope="public_profile,user_friends"
+                    callback={responseFacebook}
+                    icon="fa-facebook"/>
+                }
+            </div>
+            <div>
+                {login &&
+                <Room/>
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
